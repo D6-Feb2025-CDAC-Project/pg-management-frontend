4b2f48f162dcd000d3fcffe0b9bad1d4e34b8238
Tanvi Gavhane <gavhanetanvi@gmail.com>
2025-07-25 08:21:21 +0530

    Guest frontend : Dashboard,Amenities and Registration page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import { Route, Routes, Navigate } from 'react-router-dom'
-import UserLayout from './components/shared/UserLayout'
-import Login from './components/pages/Login'
-import PasswordReset from './components/pages/PasswordReset'
+import { Route, Routes, Navigate } from "react-router-dom";
+import UserLayout from "./components/shared/UserLayout";
+import Login from "./components/pages/Login";
+import PasswordReset from "./components/pages/PasswordReset";
 
-
-import './index.css'
+import GuestDashboard from "./components/pages/guest/GuestDashboard";
+import "./index.css";
+import Amenities from "./components/pages/guest/Amenities";
+import GuestLayout from "./components/shared/GuestLayout";
+import Registration from "./components/pages/guest/Registration";
 
 function App() {
-
   return (
     <Routes>
       {/* <Route path='/home' element={<LandingPage />} /> */}
-      <Route path='/user' element={<UserLayout />}>
-        <Route path='login' element={<Login />} />
-        <Route path='password-reset' element={<PasswordReset />} />
-        <Route index element={<Navigate to='/user/login' replace />} />
+      <Route path="/user" element={<UserLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="password-reset" element={<PasswordReset />} />
+        <Route path="registration" element={<Registration />} />
+        <Route index element={<Navigate to="/user/login" replace />} />
       </Route>
-      <Route path='/tenant' element={<UserLayout />}>
-        <Route index element={<Navigate to='/tenant/dashboard' replace />} />
+      <Route path="/tenant" element={<UserLayout />}>
+        <Route index element={<Navigate to="/tenant/dashboard" replace />} />
+      </Route>
+
+      <Route path="/guest" element={<GuestLayout />}>
+        <Route path="dashboard" element={<GuestDashboard />} />
+        <Route path="amenities" element={<Amenities />} />
+        <Route index element={<Navigate to="/guest/dashboard" replace />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/shared/GuestLayout.jsx
@@ -0,0 +1,99 @@
+import { Outlet } from "react-router";
+import React from "react";
+import logo from "../../assets/images/Logo3-cropped.svg";
+import { Link, useNavigate } from "react-router-dom";
+
+const GuestLayout = () => {
+  const navigate = useNavigate();
+
+  console.log("On guest layout page");
+
+  return (
+    <div className="h-screen overflow-y-auto hide-scrollbar bg-[#FAFAFA]">
+      {/* Navbar */}
+      <nav className="flex bg-white items-center justify-between px-4 py-2 shadow">
+        {/* left logo container */}
+        <div className="flex">
+          <img src={logo} alt="LOGO" className="w-25" />
+        </div>
+        {/* pages links */}
+        <div className="flex space-x-8 font-semibold text-black">
+          <Link to="/guest/dashboard" className="hover:text-[#E7D0F5]">
+            Home
+          </Link>
+          <Link to="#" className="hover:text-[#E7D0F5]">
+            Rooms
+          </Link>
+          <Link to="/guest/amenities" className="hover:text-[#E7D0F5]">
+            Amenities
+          </Link>
+        </div>
+        {/* login & registration button */}
+        <div className="flex justify-between">
+          <button
+            onClick={() => navigate("/user/registration")}
+            className="bg-white mr-4 text-sm text-[#3A0D5C] border-[#3A0D5C] border-2 px-3 py-1.5 rounded-lg font-medium"
+          >
+            Register
+          </button>
+          <button
+            onClick={() => navigate("/user/login")}
+            className="bg-[#6E39A3] text-sm text-white px-3 py-1.5 rounded-md font-medium "
+          >
+            Login
+          </button>
+        </div>
+      </nav>
+      <Outlet />
+      {/* footer */}
+      <footer className="bg-gray-800 text-gray-800 py-6 px-4 mt-12">
+        {/* Project Description */}
+        <p className="text-sm md:text-base text-gray-400 font-normal mb-5">
+          This educational project was developed by a dedicated team of four
+          students to help users find PGs and rental spaces easily and
+          efficiently.
+        </p>
+        <div className="max-w-6xl mx-auto text-center space-y-4 mt-5">
+          {/* Quick Links */}
+          <div className="flex justify-center flex-wrap gap-6 text-sm md:text-base  text-white">
+            <div className="flex flex-col items-center">
+              <span className="font-semibold">Home</span>
+              <span className="text-md">🏠</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="font-semibold">Rooms</span>
+              <span className="text-md">🛏️</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="font-semibold">Amenities</span>
+              <span className="text-md">🛠️</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="font-semibold">Contact</span>
+              <span className="text-md">👩🏻‍💻</span>
+            </div>
+          </div>
+
+          {/* Team Members */}
+          <div className="flex justify-center flex-wrap gap-6 mt-10 text-sm md:text-base font-semibold  text-gray-400">
+            <a href="https://www.linkedin.com/in/tanviGavhane/">
+              Tanvi Gavhane
+            </a>
+            <a href="https://www.linkedin.com/in/nivedita-bsm/">
+              Nivedita Magdum
+            </a>
+            <a href="">Prajkta Kamble</a>
+            <a href="">Shruti Naik</a>
+          </div>
+
+          {/* Copyright */}
+          <div className="border-t border-gray-400 mt-8 pt-4 text-center text-sm text-white">
+            © {new Date().getFullYear()} EasyPG. All rights reserved.
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default GuestLayout;
