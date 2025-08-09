c5c6995e9578d846bc44af3b07dbd6d20f551ddb
ivin <niveditamagdum2015@gmail.com>
2025-08-09 23:06:14 +0530

    Services url modified for accessing information for guest user without authentication from backend

--- src/services/TenantService.js
+++ src/services/TenantService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  "http://localhost:8080" || import.meta.env.VITE_API_BASE_URL;
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 const BASE_URL = `${API_BASE_URL}/tenant`;
 
 export const getAllTenants = async () => {
